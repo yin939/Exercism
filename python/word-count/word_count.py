90b9c86ad8ec9ea636a3d0ddfb2c660551b3607a
Damitte <damitte@outlook.com>
2021-05-23 00:39:17 +0800

    add word_count

--- python/word-count/word_count.py
+++ python/word-count/word_count.py
@@ -1,2 +1,18 @@
-def count_words(sentence):
-    pass
+import re
+
+
+def count_words(sentence: str):
+    sentence = re.findall(r'\w+\'?\w+|\w', sentence.lower().replace('_', ' '))
+    dict = {}
+    for word in sentence:
+        if dict.get(word):
+            dict[word] += 1
+            continue
+        dict.setdefault(word, 1)
+
+    # print(dict)
+    return dict
+
+
+if __name__ == "__main__":
+    count_words("Joe can't tell between app, apple and a.")
