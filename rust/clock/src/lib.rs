bf57058b4fd7de5672ca3eaf9ac5de77e462316f
Damitte <damitte@outlook.com>
2021-05-24 16:03:42 +0800

    some modification to clock

--- rust/clock/src/lib.rs
+++ rust/clock/src/lib.rs
@@ -1,11 +1,11 @@
 #[derive(Debug, PartialEq)]
 pub struct Clock {
-    hours: i32,
     minutes: i32,
 }
 
 const DAY_MINS: i32 = 24 * 60;
 
+/// return (hours, minutes)
 fn helper(all_mins: i32) -> (i32, i32) {
     // div_euclid 欧几里得算法 除法
     // rem_euclid 欧几里得算法 取模
@@ -14,30 +14,27 @@ fn helper(all_mins: i32) -> (i32, i32) {
             (DAY_MINS + all_mins).div_euclid(60).rem_euclid(24),
             (DAY_MINS + all_mins).rem_euclid(60),
         );
-    } else {
-        return (
-            all_mins.div_euclid(60).rem_euclid(24),
-            all_mins.rem_euclid(60),
-        );
     }
+
+    return (
+        all_mins.div_euclid(60).rem_euclid(24),
+        all_mins.rem_euclid(60),
+    );
 }
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        let all_mins = (hours * 60 + minutes) % DAY_MINS;
-        let (hours, minutes) = helper(all_mins);
-        Clock { hours, minutes }
+        let minutes = (hours * 60 + minutes).rem_euclid(DAY_MINS);
+        Clock { minutes }
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
-        let all_mins = self.hours * 60 + self.minutes;
-
-        let (hours, minutes) = helper(all_mins + (minutes % DAY_MINS));
-
-        Clock { hours, minutes }
+        let minutes = self.minutes + minutes;
+        Clock { minutes }
     }
 
     pub fn to_string(&self) -> String {
-        format!("{:0>2}:{:0>2}", self.hours, self.minutes)
+        let (hours, minutes) = helper(self.minutes);
+        format!("{:0>2}:{:0>2}", hours, minutes)
     }
 }
