f2ac3e05de575b7d66015d3da83ff0bb8806b106
Damitte <44120037+yin939@users.noreply.github.com>
2021-08-15 19:12:57 +0800

    modify code

--- python/rotational-cipher/rotational_cipher.py
+++ python/rotational-cipher/rotational_cipher.py
@@ -1,19 +1,14 @@
+from string import ascii_letters as al, ascii_uppercase as au
+
+
 def rotate(text: str, key: int) -> str:
-    key = key % 26
     text = list(text)
     for (i, c) in enumerate(text):
-        if c >= 'a' and c <= 'z':
-            asc = ord(c) + key
-            if asc > ord('z'):
-                text[i] = chr(asc - ord('z') + 96)
-            else:
-                text[i] = chr(ord(c) + key)
+        if c.isalpha():
+            if c == c.lower():
+                text[i] = al[(al.index(c) + key )% 26]
 
-        if c >= 'A' and c <= 'Z':
-            asc = ord(c) + key
-            if asc > ord('Z'):
-                text[i] = chr(asc - ord('Z') + 64)
-            else:
-                text[i] = chr(ord(c) + key)
+            if c == c.upper():
+                text[i] = au[(au.index(c) + key) % 26]
 
     return "".join(text)
