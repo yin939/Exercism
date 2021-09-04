28e557e031d424dfee7c34d888852659d0fc9acd
yin939 <44120037+yin939@users.noreply.github.com>
2021-09-04 23:56:03 +0800

    add diffie-hellman

--- rust/diffie-hellman/src/lib.rs
+++ rust/diffie-hellman/src/lib.rs
@@ -1,21 +1,29 @@
+use rand::Rng;
+
 pub fn private_key(p: u64) -> u64 {
-    unimplemented!("Pick a private key greater than 1 and less than {}", p)
+    rand::thread_rng().gen_range(2..p)
 }
 
 pub fn public_key(p: u64, g: u64, a: u64) -> u64 {
-    unimplemented!(
-        "Calculate public key using prime numbers {} and {}, and private key {}",
-        p,
-        g,
-        a
-    )
+    fast_power_modulus_algorithm(g, a, p)
 }
 
 pub fn secret(p: u64, b_pub: u64, a: u64) -> u64 {
-    unimplemented!(
-        "Calculate secret key using prime number {}, public key {}, and private key {}",
-        p,
-        b_pub,
-        a
-    )
+    fast_power_modulus_algorithm(b_pub, a, p)
+}
+
+/// 快速幂取模
+pub fn fast_power_modulus_algorithm(base: u64, exp: u64, modulus: u64) -> u64 {
+    let mut exp = exp;
+    let mut base = base;
+    let mut rel = 1;
+    while exp > 0 {
+        if exp & 1 == 1 {
+            rel = rel * base % modulus;
+        }
+        base = base * base % modulus;
+        exp >>= 1;
+    }
+
+    rel
 }
